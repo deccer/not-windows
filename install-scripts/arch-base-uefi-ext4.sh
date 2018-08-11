26b96d0510259fd211118078e75580d870ddc56b
deccer <decccer@outlook.com>
2018-08-11 21:08:03 +0200

    Fix paths

--- install-scripts/arch-base-uefi-ext4.sh
+++ install-scripts/arch-base-uefi-ext4.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/deccer/not-windows/master/base-0-variables.sh > base-0-variables.sh
-curl https://raw.githubusercontent.com/deccer/not-windows/master/base-1-ext4.sh > base-1-ext4.sh
-curl https://raw.githubusercontent.com/deccer/not-windows/master/base-2-helpers.sh > base-2-helpers.sh
-curl https://raw.githubusercontent.com/deccer/not-windows/master/base-2-chroot.sh > base-2-chroot.sh
+curl -s https://raw.githubusercontent.com/deccer/not-windows/master/install-scripts/arch/base-0-variables.sh > base-0-variables.sh
+curl -s https://raw.githubusercontent.com/deccer/not-windows/master/install-scripts/arch/base-1-ext4.sh > base-1-ext4.sh
+curl -s https://raw.githubusercontent.com/deccer/not-windows/master/install-scripts/arch/base-2-helpers.sh > base-2-helpers.sh
+curl -s https://raw.githubusercontent.com/deccer/not-windows/master/install-scripts/arch/base-2-chroot.sh > base-2-chroot.sh
+
+chmod -x base-1-ext4.sh
+chmod -x base-2-chroot.sh
+
+base-1-ext4.sh
